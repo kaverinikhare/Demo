162d059b681e8a7a0557d9c60144aae67d3e7e5b
Kaveri Nikhare <kaveri.test@yahoo.com>
2020-07-08 00:45:34 +0530

    This is my first commit for Demo ,aster branch

---
+++ src/main/java/com/qa/pages/Home.java
@@ -0,0 +1,9 @@
+package com.qa.pages;
+
+public class Home {
+	public void home()
+	{
+		System.out.println("Demo 2 page");
+	}
+
+}
---
+++ src/test/java/com/qa/tests/HomeTest.java
@@ -0,0 +1,13 @@
+package com.qa.tests;
+
+import org.testng.annotations.Test;
+
+public class HomeTest {
+	@Test
+	public void hometest()
+	{
+		System.out.println("Demo 2 test");
+	}
+
+
+}
